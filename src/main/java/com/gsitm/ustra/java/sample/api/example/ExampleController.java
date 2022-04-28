332d5849793e2f888a888f241866ac65a7440ed2
alex <alex@gsitm.com>
2022-04-28 11:03:01 +0900

    gradle wrapper 수정, maven wrapper 추가. 커스텀 응답 포맷 추가.

--- src/main/java/com/gsitm/ustra/java/sample/api/example/ExampleController.java
+++ src/main/java/com/gsitm/ustra/java/sample/api/example/ExampleController.java
@@ -1,15 +1,20 @@
 package com.gsitm.ustra.java.sample.api.example;
 
+import java.util.List;
+
 import com.gsitm.ustra.java.data.domains.PaginationList;
 import com.gsitm.ustra.java.data.domains.PaginationRequest;
-import com.gsitm.ustra.java.mvc.rest.utils.UstraRestUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Api("U.STRA Examples for API")
 @RestController
